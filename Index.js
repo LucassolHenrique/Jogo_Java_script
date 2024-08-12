15ed9c7757f2036609ab4b7e4a4a87f224bb5ddc
Lucas sol <93673324+LucassolHenrique@users.noreply.github.com>
2024-08-12 15:17:04 -0300

    subindo os 2 personagens
    ja consegue mover o player pelo A e D começando o desenvolvimento do jogo

--- Index.js
+++ Index.js
@@ -9,7 +9,7 @@ canvas.width = 1024
 canvas.height = 576
 //largura do canvas
 
-c.fillStyle = "red";
+// c.fillStyle = "red";
 //coloca a cor 
 c.fillRect(0, 0, canvas.width, canvas.height); //cria um retangulo preenchido
 //ta falando aonde deve ficar o rect no caso no mesmo tamanho do canvas
@@ -17,18 +17,111 @@ c.fillRect(0, 0, canvas.width, canvas.height); //cria um retangulo preenchido
 
 
 //orientação Objeto daqui para baixo
+
+const gravity = 0.2
+
 class Sprite {
-    constructor(position) {
+    constructor({position, velocity}) {
         this.position = position
+        this.velocity = velocity
+        this.height = 150
     }
 
     draw() {
-        c.fillRect()
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, this.height) //tamanho e lugar do player
     }
-};
 
+    update() { //atualizando o player que dermos o comando (para poder andar no w a s d)
+        this.draw()
+
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y  = 0
+        } else this.velocity.y += gravity
+    }
+}
 //criando um Objeto na posição x = 0 y = 0
-const player = new Sprite ({
-    x: 0,
-    y: 0
+
+//criando o inimigo
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+const enemy = new Sprite({
+    position: {
+        x: 400,
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+player.draw()
+
+enemy.draw()
+
+console.log(player)
+
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
+
+//animação
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+
+    player.velocity.x = 0
+    if(keys.a.pressed) {
+        player.velocity.x = -1
+    }else  if (keys.d.pressed) {
+        player.velocity.x = 1
+    }
+}
+
+animate()
+// criando o W A S D 
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = true
+            break
+        case 'a':
+            keys.a.pressed = true
+            break
+
+    }
+    console.log(event.key);
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+    }
+    console.log(event.key);
 })
