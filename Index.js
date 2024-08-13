4a2d9e477440ec5eba919593dba6980615a0f5d5
Lucas sol <93673324+LucassolHenrique@users.noreply.github.com>
2024-08-13 14:00:10 -0300

    começando o ataque
    fazendo o ataque ter animação e funcionar

--- Index.js
+++ Index.js
@@ -18,22 +18,48 @@ c.fillRect(0, 0, canvas.width, canvas.height); //cria um retangulo preenchido
 
 //orientação Objeto daqui para baixo
 
-const gravity = 0.2
+const gravity = 0.7
 
 class Sprite {
-    constructor({position, velocity}) {
+    constructor({position, velocity, color = 'red', offset}) {
         this.position = position
         this.velocity = velocity
+        this.width = 50
         this.height = 150
+        this.lastKey
+        this.attackBox = {
+            position: {
+                x: this.position.x, 
+                y: this.position.y
+            },
+            offset: offset,
+            width: 100,
+            height: 50
+        }
+        this.color = color
+        this.isAttacking
     }
 
     draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, this.height) //tamanho e lugar do player
-    }
+        c.fillStyle = this.color
+
+        c.fillRect(this.position.x, this.position.y, this.width, this.height) //tamanho e lugar do player
+        
+        //ataque do boneco
+        // if (this.isAttacking) {
+        c.fillStyle = 'green'
+        c.fillRect(
+            this.attackBox.position.x, 
+            this.attackBox.position.y,
+            this.attackBox.width,
+            this.attackBox.height)
+    // }
+}
 
     update() { //atualizando o player que dermos o comando (para poder andar no w a s d)
         this.draw()
+        this.attackBox.position.x = this.position.x - this.attackBox.offset.x   //atraso na barra de dano
+        this.attackBox.position.y = this.position.y
 
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
@@ -41,6 +67,13 @@ class Sprite {
         if (this.position.y + this.height + this.velocity.y >= canvas.height){
             this.velocity.y  = 0
         } else this.velocity.y += gravity
+    }
+
+    attack() {
+        this.isAttacking = true
+        setTimeout(() => {
+            this.isAttacking = false
+        }, 100)
     }
 }
 //criando um Objeto na posição x = 0 y = 0
@@ -54,6 +87,11 @@ const player = new Sprite({
     velocity: {
         x: 0,
         y: 0
+    },
+    offset: {
+        x:0 ,
+        y:0
+
     }
 })
 
@@ -65,6 +103,11 @@ const enemy = new Sprite({
     velocity: {
         x: 0,
         y: 0
+    },
+    color: 'blue',
+    offset: {
+        x:0 ,
+        y:0
     }
 })
 
@@ -81,6 +124,24 @@ const keys = {
     d: {
         pressed: false
     },
+    w: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    }
+}
+
+function rectangularCollision({rectangle1, rectangle}) {
+    return (
+        player.attackBox.position.x + player.attackBox.width >= enemy.position.x &&
+        player.attackBox.position.x <= enemy.position.x + enemy.width && 
+        player.attackBox.position.y + player.attackBox.height >= enemy.position.y &&
+        player.attackBox.position.y <= enemy.position.y + enemy.height 
+    )
 }
 
 //animação
@@ -92,24 +153,67 @@ function animate() {
     enemy.update()
 
     player.velocity.x = 0
-    if(keys.a.pressed) {
-        player.velocity.x = -1
-    }else  if (keys.d.pressed) {
-        player.velocity.x = 1
+    enemy.velocity.x = 0
+
+    //velocidade do player
+    if (keys.a.pressed && player.lastKey === 'a') {
+        player.velocity.x = -4
+    } else  if (keys.d.pressed && player.lastKey === 'd') {
+        player.velocity.x = 4
+    }
+
+    //velocidade do inimigo enewy
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+        enemy.velocity.x = -4
+    } else  if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+        enemy.velocity.x = 4
+    }
+
+    //detect for collision
+    if (
+        player.attackBox.position.x + player.attackBox.width >= enemy.position.x &&
+        player.attackBox.position.x <= enemy.position.x + enemy.width && 
+        player.attackBox.position.y + player.attackBox.height >= enemy.position.y &&
+        player.attackBox.position.y <= enemy.position.y + enemy.height && 
+        player.isAttacking
+    )  {
+        player.isAttacking = false
+        console.log('go');
     }
 }
 
 animate()
+
 // criando o W A S D 
 window.addEventListener('keydown', (event) => {
+    console.log(event.key);
     switch (event.key) {
         case 'd':
             keys.d.pressed = true
+            player.lastKey = 'd'
             break
         case 'a':
             keys.a.pressed = true
+            player.lastKey = 'a'
+            break
+        case 'w':
+            player.velocity.y = -10
+            break
+        case ' ':
+            player.attack()
             break
 
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true
+            enemy.lastKey = 'ArrowRight'
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            enemy.lastKey = 'ArrowLeft'
+            break
+        case 'ArrowUp':
+            enemy.velocity.y = -10
+            break
     }
     console.log(event.key);
 })
@@ -121,6 +225,15 @@ window.addEventListener('keyup', (event) => {
             break
         case 'a':
             keys.a.pressed = false
+            break
+        }
+
+    switch (event.key) {
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
             break
     }
     console.log(event.key);
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Jogo_Java_script
  criando todo um jogo em java script
+
+#dia 13_08
+
+hora 51:14
